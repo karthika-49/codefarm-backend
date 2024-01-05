ed4ffefe80512a59d151387bbadbb04154a710ad
karthika-49 <bingikarthika@gmail.com>
2024-01-05 12:33:31 +0530

    final commit

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ app.post("/login", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
